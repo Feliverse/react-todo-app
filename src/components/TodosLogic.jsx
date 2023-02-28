ed39f0b6d5c9a0e834e1ae8d5fa168ba7975383f
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-02-27 20:47:14 -0400

    add components

---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,53 @@
+import InputTodo from './InputTodo';
+import TodosList from './TodosList';
+import { useState } from 'react';
+
+const TodosLogic = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: true,
+    },
+  ]);
+  
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
+  return (
+    <div>
+      <InputTodo />
+      <TodosList todosProps={todos} setTodos={setTodos} delTodo={delTodo}/>
+    </div>
+  );
+};
+export default TodosLogic;
